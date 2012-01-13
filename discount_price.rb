aa4299f640615639f921ea1f94ab35878feb9d78
Dek Doi <9dekdoi@gmail.com>
2012-01-13 15:17:04 +0700

    discount price calculation

---
+++ discount_price.rb
@@ -0,0 +1,16 @@
+class DiscountPrice
+
+  def initialize(price, percent_discount)
+    @percent = percent_discount
+    @price = price
+  end
+
+  def discount_price
+    @price*@percent/100.0
+  end
+
+  def price_after_discount
+    @price-discount_price
+  end
+
+end
