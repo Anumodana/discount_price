9b97c25f5cbda0788192f0a608b9651ce5e4995c
Dek Doi <9dekdoi@gmail.com>
2012-01-13 15:22:46 +0700

    edit indent

--- discount_price_2.rb
+++ discount_price_2.rb
@@ -2,13 +2,13 @@ class DiscountPrice
 
   class << self
 
-  	def discount_price(price, percent_discount)
-	   price*percent_discount/100.0
-	end
+    def discount_price(price, percent_discount)
+       price*percent_discount/100.0
+    end
 
-	def price_after_discount(price, percent_discount)
-	  price-discount_price(price, percent_discount)
-	end
+    def price_after_discount(price, percent_discount)
+      price-discount_price(price, percent_discount)
+    end
 
   end
 
